a6cac86013ff4e622f756802e10019096b965530
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-07-15 11:15:06 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 
@@ -13,11 +14,13 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
         axios.get("http://localhost:8080/sales")
-        .then(response => {
-            console.log(response.data);
-        });
+            .then(response => {
+                setSales(response.data.content);
+            });
     }, []);
 
     return (
@@ -56,6 +59,25 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
+                        {sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td className="show992">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show992">{sale.visited}</td>
+                                    <td className="show992">{sale.deals}</td>
+                                    <td>R$ {sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })
+
+                        }
                         <tr>
                             <td className="show992">#341</td>
                             <td className="show576">08/07/2022</td>
